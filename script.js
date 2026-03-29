0897a7e315cfdc5751063a13d72cf0849e043db6
flexult <touma.leandre.72@gmail.com>
2026-03-29 09:13:43 +0000

    séléction du a'a'type de carte.

---
+++ script.js
@@ -0,0 +1,33 @@
+const typeSelect = document.querySelector("#typeOfCard-select");
+
+// une variable qui contient la derniere valeur de typeSelect pour s'il fait revenir en arriere
+let memoryValue = typeSelect.value;
+
+const confirmButton = document.querySelector("#confirm-btn");
+
+const forms = [
+    document.querySelector("#unit-form"),
+    document.querySelector("#event-form"),
+    document.querySelector("#amelio-form")
+];
+
+confirmButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(confirm("Attention, si vous changer de carte, les données acutelles seront effacées. Voulez vous tout de meme continuer ?")) {
+        memoryValue = typeSelect.value;
+        afficher(typeSelect.value);
+    } else {
+        typeSelect.value = memoryValue;
+    }
+})
+
+function afficher(formName) {
+    const formId = "#" + formName + "-form";
+    forms.forEach(form => {
+        if (form.id = formId) {
+            form.style.display = 'block';
+        } else {
+            form.style.display = 'none';
+        }
+    })
+}
